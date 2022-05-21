aea046f830edbcfc895d1cd2856a1c7299f10a66
João Cabral Périgo <joperigo@gmail.com>
2022-05-21 16:53:29 -0300

    Scrollbar plugin tailwind

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -34,7 +34,7 @@ export function FeedbackContentStep({
 
         <form className="my-4 w-full">
             <textarea 
-                className="min-w-[304px] w-full min-w-[112px] text-small placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-noneresize-none"
+                className="min-w-[304px] w-full min-w-[112px] text-small placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-noneresize-none scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
                 placeholder="Tell us what is happening..."
             />
         </form>
