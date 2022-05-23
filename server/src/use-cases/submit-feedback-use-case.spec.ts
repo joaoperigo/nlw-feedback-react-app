5561906e3a854b67b687aaf7ef12f5cd533f6522
João Cabral Périgo <joperigo@gmail.com>
2022-05-23 15:51:21 -0300

    All tests required on icov-report setted

--- server/src/use-cases/submit-feedback-use-case.spec.ts
+++ server/src/use-cases/submit-feedback-use-case.spec.ts
@@ -1,16 +1,40 @@
 import { SubmitFeedbackUseCase } from "./submit-feedback-use-case";
 
+const submitFeedback = new SubmitFeedbackUseCase(
+    { create: async () => {} },
+    { sendMail: async () => {} }
+)
+
 describe('Submit feedback', () => {
     it('should be able to submit a feedback', async () => {
-        const submitFeedback = new SubmitFeedbackUseCase(
-            { create: async () => {} },
-            { sendMail: async () => {} }
-        )
-        
         await expect(submitFeedback.execute({
             type: 'BUG',
             comment: 'example comment',
             screenshot: 'data:image/png;base64, 12837auy98ydqw8h',
         })).resolves.not.toThrow();
+    });
+
+    it('should not be able to submit a feedback without type', async () => {
+        await expect(submitFeedback.execute({
+            type: '',
+            comment: 'example comment',
+            screenshot: 'data:image/png;base64, 12837auy98ydqw8h',
+        })).rejects.toThrow();
+    });
+
+    it('should not be able to submit a feedback without comment', async () => {
+        await expect(submitFeedback.execute({
+            type: 'BUG',
+            comment: '',
+            screenshot: 'data:image/png;base64, 12837auy98ydqw8h',
+        })).rejects.toThrow();
+    });
+
+    it('should not be able to submit a feedback with an invalid image format', async () => {
+        await expect(submitFeedback.execute({
+            type: 'BUG',
+            comment: 'example comment',
+            screenshot: 'test.jpg',
+        })).rejects.toThrow();
     });
 });
