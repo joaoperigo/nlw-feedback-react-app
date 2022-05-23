95a7d4e34d0004031907a0e39f0d659a1a76bee1
João Cabral Périgo <joperigo@gmail.com>
2022-05-22 23:29:41 -0300

    Created and started setting repository

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -16,13 +16,7 @@ const transport = nodemailer.createTransport({
 routes.post('/feedbacks', async (req, res) => {
     const { type, comment, screenshot } = req.body;
 
-    const feedback = await prisma.feedback.create({
-        data: {
-            type,
-            comment,
-            screenshot,
-        }
-    })
+    const feedback = 
 
     transport.sendMail({
         from: 'Equipe Feedget <oi@feedget.com>',
