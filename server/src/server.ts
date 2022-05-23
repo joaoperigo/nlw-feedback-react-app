1e408241c2a1c1c4a285650cf492daa6106eac85
João Cabral Périgo <joperigo@gmail.com>
2022-05-22 22:53:56 -0300

    Setted email sending

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,6 +6,15 @@ const app = express();
 
 app.use(express.json());
 
+const transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "d87dee259f59d2",
+      pass: "cc817041c9b342"
+    }
+  });
+
 app.post('/feedbacks', async (req, res) => {
     const { type, comment, screenshot } = req.body;
 
@@ -16,6 +25,18 @@ app.post('/feedbacks', async (req, res) => {
             screenshot,
     }
 })
+
+    transport.sendMail({
+        from: 'Equipe Feedget <oi@feedget.com>',
+        to: 'João <joperigo@gmail.com>',
+        subject: 'Novo feedback',
+        html: [
+            `<div style="font-family: sans-serif; font-size: 16px; color: #333333;">`,
+            `<p>Tipo do feedback: ${type}</p>`,
+            `<p>Comentário: ${comment}</p>`,
+            `</div>`,
+        ].join('\n')
+    })
 
     return res.status(201).json({ data: feedback });
 })
